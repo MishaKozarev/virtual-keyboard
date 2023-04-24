51b03eb19b52f3af8abf5305c8c91950cbbddbe3
MishaKozarev <mishaaaaa1986@mail.ru>
2023-04-25 02:08:46 +0300

    feat: add and import keyboard.js

---
+++ app.js
@@ -0,0 +1,3 @@
+import KEYBOARD from './js/keyboard.js';
+const body = document.querySelector('.body');
+body.insertAdjacentHTML('afterbegin', KEYBOARD);
---
+++ js/keyboard.js
@@ -0,0 +1,56 @@
+const KEYBOARD = `
+<div class="wrapper">
+  <label for="input-screen">Virtual Keyboard:</label>
+  <textarea class = "input-screen" id = "input-screen" name = "input-screen"></textarea>
+  <div class="keyboard">
+    <div class="keyboard__row">
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    <div class="button"></div>
+    </div>
+  </div>
+</div>
+`;
+export default KEYBOARD;
