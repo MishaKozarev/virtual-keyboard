36ba5e7ee77303fe658117da1cdff3d50d2e6196
MishaKozarev <mishaaaaa1986@mail.ru>
2023-04-28 02:42:36 +0300

    feat: add language change and CapsLock function on keyboard

--- app.js
+++ app.js
@@ -4,71 +4,134 @@ import LAYOT_RU from './js/layot-ru.js';
 
 const body = document.querySelector('.body');
 body.insertAdjacentHTML('afterbegin', KEYBOARD);
-const inputScreen = document.querySelector('.input-screen');
+const DISPLEY = document.querySelector('.input-screen');
 const keyboardRow = document.querySelector('.keyboard__row');
+const KEYS = document.querySelectorAll('.key');
+const KEY_DELETE = document.querySelector('.key-delete');
+const SHIFT = document.getElementById('shift');
 
 const LAYOT_EN_NORMAL = LAYOT_EN.normal.split(' ');
 const LAYOT_EN_CAPSLOCK = LAYOT_EN.capsLock.split(' ');
+const LAYOT_EN_SHIFT = LAYOT_EN.shift.split(' ');
+const LAYOT_EN_CAPSLOCK_SHIFT = LAYOT_EN.capsLockAndShift.split(' ');
 const LAYOT_RU_NORMAL = LAYOT_RU.normal.split(' ');
 const LAYOT_RU_CAPSLOCK = LAYOT_RU.capsLock.split(' ');
+const LAYOT_RU_SHIFT = LAYOT_RU.shift.split(' ');
+const LAYOT_RU_CAPSLOCK_SHIFT = LAYOT_EN.capsLockAndShift.split(' ');
+
 let lang = 'en';
 let flag = false;
+let count  =[];
+DISPLEY.focus();
 
+// Добавить раскладку
+function addLayot(layot) {
+  for (let i = 0; i < layot.length; i++) {
+    keyboardRow.children[i].innerHTML = layot[i];
+  }
+}
 
-function init (){
-    addLayot(LAYOT_EN_NORMAL);
+// Инициализация
+function init() {
+  addLayot(LAYOT_EN_NORMAL);
 }
 init();
 
-function addLayot(layot) {
-    for (let i = 0; i < layot.length; i ++){
-        keyboardRow.children[i].innerHTML = layot[i]
-    }
-}
+//Подсветка клавиатуро
+DISPLEY.onkeydown = function (event) {
+  KEYS.forEach((element) => {
+    element.classList.remove('active');
+  });
+  document.querySelector(`.key[data-key="${event.code}"]`).classList.add('active');
+};
 
-inputScreen.onkeydown = function (event){
-    document.querySelectorAll('.key').forEach((el) => {
-        el.classList.remove('active');
-    });
-    document.querySelector('.key[data-key="'+event.code+'"]').classList.add('active');
-}
 
-document.querySelectorAll('.key').forEach((el) =>{
-    el.onclick = function (){
-        document.querySelectorAll('.key').forEach((el) =>{
-            el.classList.remove('active');
-        });
-        let code = this.getAttribute('data-key')
-        this.classList.add('active');
-        inputScreen.textContent = el.textContent;
-        console.log(el.textContent);
+// Смена языка клавиатурой
+window.onkeydown = function (e) {
+  if (lang === 'en') {
+    if (e.code === 'ShiftLeft') flag = true;
+    if (e.code === 'AltLeft' && flag) {
+      addLayot(LAYOT_RU_NORMAL);
+      flag = false;
+      lang = 'ru';
     }
+  } else {
+    if (e.code === 'ShiftLeft') flag = true;
+    if (e.code === 'AltLeft' && flag) {
+      addLayot(LAYOT_EN_NORMAL);
+      flag = false;
+      lang = 'en';
+    }
+  }
+};
+
+// CapsLock клавиатурой
+window.addEventListener('keyup', (event) => {
+  if (lang === 'en') {
+    if (event.getModifierState('CapsLock')) {
+      addLayot(LAYOT_EN_CAPSLOCK);
+    } else {
+      addLayot(LAYOT_EN_NORMAL);
+    }
+  } else if (event.getModifierState('CapsLock')) {
+    addLayot(LAYOT_RU_CAPSLOCK);
+  } else {
+    addLayot(LAYOT_RU_NORMAL);
+  }
 });
 
 
-inputScreen.onkeydown = function (e){
-    if (lang === 'en'){
-        if (e.code === "ShiftLeft") flag = true;
-        if (e.code === "AltLeft" && flag ) {
-            addLayot(LAYOT_RU_NORMAL);
-            flag = false;
-            lang = 'ru'
-        }
-    }else{
-        if (e.code === "ShiftLeft") flag = true;
-        if (e.code === "AltLeft" && flag ) {
-            addLayot(LAYOT_EN_NORMAL);
-            flag = false;
-            lang = 'en'
-        }
-    }
-}
+// // Shift клавиатурой
+// window.onkeydown = function (event) {
+//     if (lang === 'en') {
+//       if (event.code === 'ShiftLeft') {
+//         addLayot(LAYOT_EN_SHIFT);
+//       } else {
+//         addLayot(LAYOT_EN_NORMAL);
+//       }
+//     }else{
+//       if (event.code === 'ShiftLeft') {
+//         addLayot(LAYOT_RU_SHIFT);
+//       } else {
+//         addLayot(LAYOT_RU_NORMAL);
+//         }
+//     }
+//   };
 
 
-inputScreen.addEventListener("keyup", function(event) {
-    if (event.getModifierState("CapsLock")) {
-        addLayot(LAYOT_EN_CAPSLOCK);
-    } else {
-        addLayot(LAYOT_EN_NORMAL);
-    }
+
+// Shift + CapsLock клавиатурой
+// window.onkeydown = function (event) {
+//     if (lang === 'en') {
+//         if (event.getModifierState('CapsLock')){
+//             if (event.code === 'ShiftLeft') {
+//                 addLayot(LAYOT_EN_CAPSLOCK_SHIFT);
+//             }
+//         }
+//     }else{
+//         if (event.getModifierState('CapsLock')){
+//             if (event.code === 'ShiftLeft') {
+//                 addLayot(LAYOT_RU_CAPSLOCK_SHIFT);
+//             }
+//         }
+//     }
+// }
+
+
+document.querySelectorAll('.key').forEach((element) => {
+    element.onclick = function () {
+      document.querySelectorAll('.key').forEach((element) => {
+        element.classList.remove('active');
+      });
+      const code = this.getAttribute('data-key');
+      this.classList.add('active');
+      DISPLEY.value += element.textContent
+      count = DISPLEY.value.split('')
+      console.log(code);
+    };
+});
+
+  KEY_DELETE.addEventListener('click',() => {
+    count.pop();
+    DISPLEY.value = count.join('');
   });
--- js/keyboard.js
+++ js/keyboard.js
@@ -4,70 +4,70 @@ const KEYBOARD = `
   <textarea class = "input-screen" id = "input-screen" name = "input-screen"></textarea>
   <div class="keyboard">
     <div class="keyboard__row">
-    <div class="key" data-key="Backquote"></div>
-    <div class="key" data-key="Digit1"></div>
-    <div class="key" data-key="Digit2"></div>
-    <div class="key" data-key="Digit3">></div>
-    <div class="key" data-key="Digit4"></div>
-    <div class="key" data-key="Digit5"></div>
-    <div class="key" data-key="Digit6"></div>
-    <div class="key" data-key="Digit7"></div>
-    <div class="key" data-key="Digit8"></div>
-    <div class="key" data-key="Digit9"></div>
-    <div class="key" data-key="Digit0"></div>
-    <div class="key" data-key="Minus"></div>
-    <div class="key" data-key="Equal"></div>
-    <div class="key key-double" data-key="Backspace"></div>
-    <div class="key key-double" data-key="Tab"></div>
-    <div class="key" data-key="KeyQ"></div>
-    <div class="key" data-key="KeyW"></div>
-    <div class="key" data-key="KeyE"></div>
-    <div class="key" data-key="KeyR"></div>
-    <div class="key" data-key="KeyT"></div>
-    <div class="key" data-key="KeyY"></div>
-    <div class="key" data-key="KeyU"></div>
-    <div class="key" data-key="KeyI"></div>
-    <div class="key" data-key="KeyO"></div>
-    <div class="key" data-key="KeyP"></div>
-    <div class="key" data-key="BracketLeft"></div>
-    <div class="key" data-key="BracketRight"></div>
-    <div class="key" data-key="Backslash"></div>
-    <div class="key key-double" data-key="CapsLock"></div>
-    <div class="key" data-key="KeyA"></div>
-    <div class="key" data-key="KeyS"></div>
-    <div class="key" data-key="KeyD"></div>
-    <div class="key" data-key="KeyF"></div>
-    <div class="key" data-key="KeyG"></div>
-    <div class="key" data-key="KeyH"></div>
-    <div class="key" data-key="KeyJ">></div>
-    <div class="key" data-key="KeyK"></div>
-    <div class="key" data-key="KeyL"></div>
-    <div class="key" data-key="Semicolon"></div>
-    <div class="key" data-key="Quote"></div>
-    <div class="key key-double" data-key="Enter"></div>
-    <div class="key key-double" data-key="ShiftLeft"></div>
-    <div class="key" data-key="KeyZ"></div>
-    <div class="key" data-key="KeyX"></div>
-    <div class="key" data-key="KeyC"></div>
-    <div class="key" data-key="KeyV"></div>
-    <div class="key" data-key="KeyB"></div>
-    <div class="key" data-key="KeyN"></div>
-    <div class="key" data-key="KeyM"></div>
-    <div class="key" data-key="Comma"></div>
-    <div class="key" data-key="Period"></div>
-    <div class="key" data-key="Slash"></div>
-    <div class="key" data-key="ShiftRight"></div>
-    <div class="key" data-key="ArrowUp"></div>
-    <div class="key" data-key="Delete"></div>
-    <div class="key" data-key="ControlLeft"></div>
-    <div class="key" data-key="Win"></div>
-    <div class="key" data-key="AltLeft"></div>
-    <div class="key key-space" data-key="Space"></div>
-    <div class="key" data-key="AltRight"></div>
-    <div class="key" data-key="Space"></div>
-    <div class="key" data-key="ArrowLeft"></div>
-    <div class="key" data-key="ArrowDown"></div>
-    <div class="key" data-key="ArrowRight"></div>
+      <div class="key" data-key="Backquote"></div>
+      <div class="key" data-key="Digit1"></div>
+      <div class="key" data-key="Digit2"></div>
+      <div class="key" data-key="Digit3">></div>
+      <div class="key" data-key="Digit4"></div>
+      <div class="key" data-key="Digit5"></div>
+      <div class="key" data-key="Digit6"></div>
+      <div class="key" data-key="Digit7"></div>
+      <div class="key" data-key="Digit8"></div>
+      <div class="key" data-key="Digit9"></div>
+      <div class="key" data-key="Digit0"></div>
+      <div class="key" data-key="Minus"></div>
+      <div class="key" data-key="Equal"></div>
+      <div class="key key-double" data-key="Backspace"></div>
+      <div class="key key-double" data-key="Tab"></div>
+      <div class="key" data-key="KeyQ"></div>
+      <div class="key" data-key="KeyW"></div>
+      <div class="key" data-key="KeyE"></div>
+      <div class="key" data-key="KeyR"></div>
+      <div class="key" data-key="KeyT"></div>
+      <div class="key" data-key="KeyY"></div>
+      <div class="key" data-key="KeyU"></div>
+      <div class="key" data-key="KeyI"></div>
+      <div class="key" data-key="KeyO"></div>
+      <div class="key" data-key="KeyP"></div>
+      <div class="key" data-key="BracketLeft"></div>
+      <div class="key" data-key="BracketRight"></div>
+      <div class="key" data-key="Backslash"></div>
+      <div class="key key-double" data-key="CapsLock"></div>
+      <div class="key" data-key="KeyA"></div>
+      <div class="key" data-key="KeyS"></div>
+      <div class="key" data-key="KeyD"></div>
+      <div class="key" data-key="KeyF"></div>
+      <div class="key" data-key="KeyG"></div>
+      <div class="key" data-key="KeyH"></div>
+      <div class="key" data-key="KeyJ">></div>
+      <div class="key" data-key="KeyK"></div>
+      <div class="key" data-key="KeyL"></div>
+      <div class="key" data-key="Semicolon"></div>
+      <div class="key" data-key="Quote"></div>
+      <div class="key key-double" data-key="Enter"></div>
+      <div class="key key-double" id='shift' data-key="ShiftLeft"></div>
+      <div class="key" data-key="KeyZ"></div>
+      <div class="key" data-key="KeyX"></div>
+      <div class="key" data-key="KeyC"></div>
+      <div class="key" data-key="KeyV"></div>
+      <div class="key" data-key="KeyB"></div>
+      <div class="key" data-key="KeyN"></div>
+      <div class="key" data-key="KeyM"></div>
+      <div class="key" data-key="Comma"></div>
+      <div class="key" data-key="Period"></div>
+      <div class="key" data-key="Slash"></div>
+      <div class="key" data-key="ShiftRight"></div>
+      <div class="key" data-key="ArrowUp"></div>
+      <div class="key" id='key-delete' data-key="Delete"></div>
+      <div class="key" data-key="ControlLeft"></div>
+      <div class="key" data-key="Win"></div>
+      <div class="key" data-key="AltLeft"></div>
+      <div class="key key-space" data-key="Space"></div>
+      <div class="key" data-key="AltRight"></div>
+      <div class="key" data-key="Space"></div>
+      <div class="key" data-key="ArrowLeft"></div>
+      <div class="key" data-key="ArrowDown"></div>
+      <div class="key" data-key="ArrowRight"></div>
     </div>
   </div>
 </div>
